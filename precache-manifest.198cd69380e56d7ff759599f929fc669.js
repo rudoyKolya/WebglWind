99c41abb6f3bafcbe877ac5e224c3dde013731f1
nickolayRudy <nickolay.rudy@i4-insight.com>
2023-07-03 19:25:40 +0200

    Updates

---
+++ precache-manifest.198cd69380e56d7ff759599f929fc669.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ca44b3ecc34e704a7670ab14304988a",
+    "url": "/WebglWind/index.html"
+  },
+  {
+    "revision": "c96266ad1f00ac6c5a48",
+    "url": "/WebglWind/static/css/main.64a65e0f.chunk.css"
+  },
+  {
+    "revision": "9c5f6d2b4c68508a0086",
+    "url": "/WebglWind/static/js/2.c5b6dc52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WebglWind/static/js/2.c5b6dc52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c96266ad1f00ac6c5a48",
+    "url": "/WebglWind/static/js/main.24338e78.chunk.js"
+  },
+  {
+    "revision": "348e2f60e6b9e7ed65c6",
+    "url": "/WebglWind/static/js/runtime-main.edfa7858.js"
+  },
+  {
+    "revision": "3a65a12c3edb942b4109c334698eb462",
+    "url": "/WebglWind/static/media/2016112000.3a65a12c.png"
+  },
+  {
+    "revision": "d92cb4ab34d782b5062c34214abafc93",
+    "url": "/WebglWind/static/media/2016112106.d92cb4ab.png"
+  },
+  {
+    "revision": "5acbc2193d433e212323d42bcfc20282",
+    "url": "/WebglWind/static/media/2016112118.5acbc219.png"
+  }
+]);
